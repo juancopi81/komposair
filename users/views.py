90acb9a6a8183712513b4f6bbc5556e47fcfb4cc
juancopi81 <juancopi_81@hotmail.com>
2020-11-05 11:45:47 -0500

    Change indentation tabs to spaces

--- users/views.py
+++ users/views.py
@@ -5,41 +5,40 @@ from django.contrib.auth.decorators import login_required
 
 
 def register(request):
-	if request.method == "POST":
-		form = UserRegisterForm(request.POST)
-		if form.is_valid():
-			form.save()
-			messages.success(request, "Your account has been created! You are now able to log in.")
-			return redirect("login")
-	else:
-		form = UserRegisterForm()
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your account has been created! You are now able to log in.")
+            return redirect("login")
+    else:
+        form = UserRegisterForm()
 
-	description = "Register in Komposair and start creating melodies our of your motifs using AI"
-	return render(request, "users/register.html", {"form": form, "title": "Register", "description": description})
+    description = "Register in Komposair and start creating melodies our of your motifs using AI"
+    return render(request, "users/register.html", {"form": form, "title": "Register", "description": description})
 
 
 @login_required
 def profile(request):
+    if request.method == "POST":
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpadteForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, "Your account has been updated")
+            return redirect("profile")
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpadteForm(instance=request.user.profile)
 
-	if request.method == "POST":
-		u_form = UserUpdateForm(request.POST, instance=request.user)
-		p_form = ProfileUpadteForm(request.POST,
-								   request.FILES,
-								   instance=request.user.profile)
-		if u_form.is_valid() and p_form.is_valid():
-			u_form.save()
-			p_form.save()
-			messages.success(request, "Your account has been updated")
-			return redirect("profile")
-
-	else:
-		u_form = UserUpdateForm(instance=request.user)
-		p_form = ProfileUpadteForm(instance=request.user.profile)
-
-	context = {
-		"u_form": u_form,
-		"p_form": p_form,
-		"title": "Profile",
-		"description": "Update your profile in Komposair"
-	}
-	return render(request, "users/profile.html", context)
+    context = {
+        "u_form": u_form,
+        "p_form": p_form,
+        "title": "Profile",
+        "description": "Update your profile in Komposair"
+    }
+    return render(request, "users/profile.html", context)
+    
