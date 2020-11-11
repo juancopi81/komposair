1408320566cd5dada0ade87b5d58480f5f52ba7f
juancopi81 <juancopi_81@hotmail.com>
2020-11-11 17:08:58 -0500

    Add scroll to top when generating melodies and restart apache2 on ci.yml

--- melody/static/melody/melody.js
+++ melody/static/melody/melody.js
@@ -168,6 +168,10 @@ document.addEventListener('DOMContentLoaded', () => {
 		// Disable button while processing
 		document.getElementById('input-generate').disabled = true;
 
+		// Go to top of the page
+		document.body.scrollTop = 0; // For Safari
+		document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+
 		// Get bpm
 		let bpm = parseInt(document.getElementById('bpm').value);
 
