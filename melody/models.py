419cc57884c08231c8b8626879d1cdadab55065a
juancopi81 <juancopi_81@hotmail.com>
2020-11-18 19:03:19 -0500

    Fix most flake8 warnings in melody/tests, models and utils

--- melody/models.py
+++ melody/models.py
@@ -3,30 +3,33 @@ from django.conf import settings
 from django.utils import timezone
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+
 # Create your models here.
 class Melody(models.Model):
-	notes = models.JSONField()
-	bpm = models.IntegerField()
-	aimodel = models.CharField(max_length=200)
-	score = models.IntegerField(default=0)
-	person = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="melodies")
-	date_created = models.DateTimeField(default=timezone.now)
+    notes = models.JSONField()
+    bpm = models.IntegerField()
+    aimodel = models.CharField(max_length=200)
+    score = models.IntegerField(default=0)
+    person = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="melodies")
+    date_created = models.DateTimeField(default=timezone.now)
 
-	def __str__(self):
-		return str(self.id)
+    def __str__(self):
+        return str(self.id)
+
 
 class Vote(models.Model):
 
-	user_score = models.IntegerField(validators=[MaxValueValidator(1), MinValueValidator(-1)])
-	melody = models.ForeignKey(Melody, on_delete=models.CASCADE, related_name="scores")
-	person = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="voters")
+    user_score = models.IntegerField(validators=[MaxValueValidator(1), MinValueValidator(-1)])
+    melody = models.ForeignKey(Melody, on_delete=models.CASCADE, related_name="scores")
+    person = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="voters")
 
-	def __str__(self):
-		return f"{self.person} - {self.melody} - {self.user_score}"
+    def __str__(self):
+        return f"{self.person} - {self.melody} - {self.user_score}"
+
 
 class Comment(models.Model):
 
-	posted_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="reviews")
-	melody = models.ForeignKey(Melody, on_delete=models.CASCADE, related_name="comments")
-	date_posted = models.DateTimeField(default=timezone.now)
-	comment = models.TextField()
+    posted_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="reviews")
+    melody = models.ForeignKey(Melody, on_delete=models.CASCADE, related_name="comments")
+    date_posted = models.DateTimeField(default=timezone.now)
+    comment = models.TextField()
--- melody/utils.py
+++ melody/utils.py
@@ -3,216 +3,209 @@ import os
 import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.utils import to_categorical
-from django.conf import settings
 import music21 as m21
 
 SEQUENCE_LENGTH = 64
 MAPPING_PATH = "mapping.json"
 
+
 class MelodyGenerator:
-	""" 
-	Class to generate melodies based on h5 model
+    """
+    Class to generate melodies based on h5 model
 
-	...
+    ...
 
-	Attributes:
-	----------
-	model_path : str
-		string where the model is stored
-	model : h5 model
-		h5 model to generate a melody
+    Attributes:
+    ----------
+    model_path : str
+        string where the model is stored
+    model : h5 model
+        h5 model to generate a melody
 
-	Methods:
-	-------
-	generate_melody(seed, num_steps, max_sequence_length, temperature)
-		generates melody based on the seed usind the given model 
-	"""
+    Methods:
+    -------
+    generate_melody(seed, num_steps, max_sequence_length, temperature)
+    generates melody based on the seed usind the given model
+    """
 
-	def __init__(self, model_path="model.h5"):
+    def __init__(self, model_path="model.h5"):
 
-		DIRNAME = os.path.dirname(__file__)
+        DIRNAME = os.path.dirname(__file__)
 
-		self.model_path = os.path.join(DIRNAME, model_path)
+        self.model_path = os.path.join(DIRNAME, model_path)
 
-		self.model = load_model(self.model_path)
+        self.model = load_model(self.model_path)
 
-		mapping_path = os.path.join(DIRNAME, MAPPING_PATH)
+        mapping_path = os.path.join(DIRNAME, MAPPING_PATH)
 
-		with open(mapping_path, "r") as fp:
-			self._mappings = json.load(fp)
+        with open(mapping_path, "r") as fp:
+            self._mappings = json.load(fp)
 
-		self._start_symbols = ["/"] * SEQUENCE_LENGTH
+        self._start_symbols = ["/"] * SEQUENCE_LENGTH
 
+    def generate_melody(self, seed, num_steps, max_sequence_length, temperature):
 
-	def generate_melody(self, seed, num_steps, max_sequence_length, temperature):
+        # create seed with start symbols
+        seed = seed.split()
+        melody = seed
+        seed = self._start_symbols + seed
 
-		# create seed with start symbols
-		seed = seed.split()
-		melody = seed
-		seed = self._start_symbols + seed
+        # Map seed to integers
+        seed = [self._mappings[symbol] for symbol in seed]
 
-		# Map seed to integers
-		seed = [self._mappings[symbol] for symbol in seed]
+        for _ in range(num_steps):
+            # Limit the seed to max_sequence_length
+            seed = seed[-max_sequence_length:]
 
-		for _ in range(num_steps):
-			
-			# Limit the seed to max_sequence_length
-			seed = seed[-max_sequence_length:]
+            # One-hot encode the seed
+            onehot_seed = to_categorical(seed, num_classes=len(self._mappings))
+            # (New dim, max_sequence_length, number of symbols in the vocab)
+            # Add third dimension -> for keras expect multiple samples
+            onehot_seed = onehot_seed[np.newaxis, ...]
 
-			# One-hot encode the seed
-			onehot_seed = to_categorical(seed, num_classes = len(self._mappings))
-			#(New dim, max_sequence_length, number of symbols in the vocab) Add third dimension -> for keras expect multiple samples
-			onehot_seed = onehot_seed[np.newaxis,...]
+            # Make a prediction
+            probabilities = self.model.predict(onehot_seed)[0]
 
-			# Make a prediction
-			probabilities = self.model.predict(onehot_seed)[0]
-			
-			# [0.1, 0.2, 0.1, 0.6] add to 1
-			output_int = self._sample_with_temperature(probabilities, temperature)
+            # [0.1, 0.2, 0.1, 0.6] add to 1
+            output_int = self._sample_with_temperature(probabilities, temperature)
 
-			# updaate the seed
-			seed.append(output_int)
+            # updaate the seed
+            seed.append(output_int)
 
-			# Map int to our encoding
-			output_symbol = [k for k, v in self._mappings.items() if v == output_int][0]
+            # Map int to our encoding
+            output_symbol = [k for k, v in self._mappings.items() if v == output_int][0]
 
-			# Check whether we are at end of melody
-			if output_symbol == "/":
-				break
-			# Update the meelody
-			else:
-				melody.append(output_symbol)
+            # Check whether we are at end of melody
+            if output_symbol == "/":
+                break
+            # Update the meelody
+            else:
+                melody.append(output_symbol)
 
-		return melody
+        return melody
 
+    def _sample_with_temperature(self, probabilities, temperature):
+        # Temperature value -> infinite, random picking one of the probabilities
+        # Temperature value -> 0, higher gets 1 other 0
+        # Temperature value -> 1, nothing changes in probabilities
 
-	def _sample_with_temperature(self, probabilities, temperature):
-		# Temperature value -> infinite, random picking one of the probabilities
-		# Temperature value -> 0, higher gets 1 other 0
-		# Temperature value -> 1, nothing changes in probabilities
+        predictions = np.log(probabilities.clip(min=0.00000000001)) / temperature
+        probabilities = np.exp(predictions) / np.sum(np.exp(predictions))
 
-		predictions = np.log(probabilities.clip(min=0.00000000001)) / temperature
-		probabilities = np.exp(predictions) / np.sum(np.exp(predictions))
+        choices = range(len(probabilities))    # [0, 1, 2, 3]
+        index = np.random.choice(choices, p=probabilities)
 
-		choices = range(len(probabilities)) # [0, 1, 2, 3]
-		index = np.random.choice(choices, p=probabilities)
+        return index
 
-		return index
+    def save_melody(self, melody, step_duration=0.25, format="midi", file_name="mel1.mid", bpm=90):
 
-	def save_melody(self, melody, step_duration=0.25, format="midi", file_name="mel1.mid", bpm=90):
+        # Create a music21 stream
+        stream = m21.stream.Stream()
 
-		# Create a music21 stream
-		stream = m21.stream.Stream()
+        mm = m21.tempo.MetronomeMark(number=bpm)
+        stream.append(mm)
 
-		mm = m21.tempo.MetronomeMark(number=bpm)
-		stream.append(mm)
+        # Parse all the symbols in the melody and create note/rest objects
+        # 60 _ _ _ r _ 62 _
+        start_symbol = None
+        step_counter = 1
 
-		# Parse all the symbols in the melody and create note/rest objects
-		# 60 _ _ _ r _ 62 _
-		start_symbol = None
-		step_counter = 1
+        for i, symbol in enumerate(melody):
 
-		for i, symbol in enumerate(melody):
+            # Handle case we have note/rest
+            if symbol != "_" or i + 1 == len(melody):
 
-			# Handle case we have note/rest
-			if symbol != "_" or i + 1 == len(melody):
-				
-				# Ensure we are dealing with note/rest beyond first one
-				if start_symbol is not None:
+                # Ensure we are dealing with note/rest beyond first one
+                if start_symbol is not None:
 
-					# Calculate duration
-					quarter_length_duration = step_duration * step_counter # 0.25 * 4
- 
-					# Handle rest
-					if start_symbol == "r":
-						m21_event = m21.note.Rest(quarterLength = quarter_length_duration)
+                    # Calculate duration
+                    quarter_length_duration = step_duration * step_counter    # 0.25 * 4
 
-					# Handle note
-					else:
-						m21_event = m21.note.Note(int(start_symbol), quarterLength = quarter_length_duration)
+                    # Handle rest
+                    if start_symbol == "r":
+                        m21_event = m21.note.Rest(quarterLength=quarter_length_duration)
 
-					stream.append(m21_event)
+                    # Handle note
+                    else:
+                        m21_event = m21.note.Note(int(start_symbol), quarterLength=quarter_length_duration)
 
-					# Reset step counter
-					step_counter = 1
+                    stream.append(m21_event)
 
-				start_symbol = symbol
+                    # Reset step counter
+                    step_counter = 1
 
+                start_symbol = symbol
 
-			# Handle case we have prolongation sign -> "_"
-			else:
-				step_counter += 1
+            # Handle case we have prolongation sign -> "_"
+            else:
+                step_counter += 1
 
-		# Write the m21 stream to midi 
-		midi = stream.write(format)
+        # Write the m21 stream to midi
+        midi = stream.write(format)
 
-
-		# Return the midi file object
-		return midi
+        # Return the midi file object
+        return midi
 
 
-def parse_motif(motif, time_step = 0.25, parser="series"):
-	""" Parses the given notes to a string used by the model or by magenta
+def parse_motif(motif, time_step=0.25, parser="series"):
+    """ Parses the given notes to a string used by the model or by magenta
 
-	Parameters
-	----------
-	motif : str
-		The notes given in string format
-	time_step : float
-		Based duration, default 16th note
-	parser : string
-		String to decide to parse the notes so 
-		they can be feed to the model or to magenta
+    Parameters
+    ----------
+    motif : str
+        The notes given in string format
+    time_step : float
+        Based duration, default 16th note
+    parser : string
+        String to decide to parse the notes so
+        they can be feed to the model or to magenta
 
-	Returns
-	-------
-	string/dict
-		a string (time series ) or dict that can be used to feed the model
-		or magenta
-	"""
+    Returns
+    -------
+    string/dict
+        a string (time series ) or dict that can be used to feed the model
+        or magenta
+    """
 
-	# Initialize and empty list and total duration to zero
-	encoded_motif = []
-	total_duration = 0
+    # Initialize and empty list and total duration to zero
+    encoded_motif = []
+    total_duration = 0
 
-	for notes in motif:
-		# Handle notes
-		if "r" not in notes["duration"]:
-			# Convert pitch to its midi number
-			symbol = m21.pitch.Pitch(notes['note']).midi
+    for notes in motif:
+        # Handle notes
+        if "r" not in notes["duration"]:
+            # Convert pitch to its midi number
+            symbol = m21.pitch.Pitch(notes['note']).midi
 
-		# Handle rests
-		else:
-			symbol = "r"
+        # Handle rests
+        else:
+            symbol = "r"
 
-		# Convert the note/rest into time series notation
-		
-		# Remove "r" and "." from the duration
-		duration = float(notes["duration"].replace(".", "").replace("r", ""))
+        # Convert the note/rest into time series notation
 
-		# Calculate duration in quarter length
-		duration = 4.0 / duration
+        # Remove "r" and "." from the duration
+        duration = float(notes["duration"].replace(".", "").replace("r", ""))
 
-		total_duration += duration
+        # Calculate duration in quarter length
+        duration = 4.0 / duration
 
-		# Update duration if note is dotted
-		if (notes['dot']):
-			duration += duration / 2.0
-		
-		# Calculate number of sixteenth steps
-		steps = int(duration / time_step)
+        total_duration += duration
 
-		# Create "_" based on number of steps
-		for step in range(steps):
-			if step == 0:
-				encoded_motif.append(symbol)
-			else:
-				encoded_motif.append("_")
+        # Update duration if note is dotted
+        if (notes['dot']):
+            duration += duration / 2.0
 
-	# Cast encoded_motif to string
-	encoded_motif = " ".join(map(str, encoded_motif))
+        # Calculate number of sixteenth steps
+        steps = int(duration / time_step)
 
-	return encoded_motif, total_duration
+        # Create "_" based on number of steps
+        for step in range(steps):
+            if step == 0:
+                encoded_motif.append(symbol)
+            else:
+                encoded_motif.append("_")
 
+    # Cast encoded_motif to string
+    encoded_motif = " ".join(map(str, encoded_motif))
 
-
+    return encoded_motif, total_duration
